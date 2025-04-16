5db2b6e9cc36faff41831f8c0ee29c9acd49615d
LFCKJ <k71689@naver.com>
2025-04-16 14:36:35 +0900

    todolist,insert

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Todoinsert from './components/Todoinsert';
+import TodoList from './components/TodoList';
+
+function App() {
+  //Logic area
+
+  
+
+
+//view area
+  return (
+    <div>
+      <Todoinsert/>
+      <TodoList/>    
+    </div>
+  );
+}
+export default App
---
+++ src/components/Todoinsert.jsx
@@ -0,0 +1,15 @@
+function Todoinsert() {
+
+
+
+    return (
+    <form>
+     <input type="text" 
+     placeholder="할 일을 입력하세요."/>
+     <button type="submit">등록</button>
+    </form>
+    
+);
+}
+
+export default Todoinsert;
