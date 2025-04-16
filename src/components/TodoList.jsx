cc4eacb04143d1a8598afa2145551b9ba868c183
LFCKJ <k71689@naver.com>
2025-04-16 15:12:06 +0900

    데이터 추가가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 function TodoList() {
+//Logic area
+    const [todos, _setTodos] = useState([
+        {
+            id: 1,
+            text: '할 일 1',
+            done: false
+        },
+        {
+            id: 2,
+            text: '할 일 2',
+            done: true
+        },
+        {
+            id: 3,
+            text: '할 일 3',
+            done: false
+        }
+    ]); 
+      
+
+
+
     return ( 
     <ul>
-        <li>아이템1</li>
-        <li>아이템2</li>
-        <li>아이템3</li>
+          {todos.map((item)=>(
+            <li>{item.text}</li>
+          ))}
     
     </ul> );
 }
 
-export default TodoList;
+export default TodoList;  
